6c05dafaecb85d11e60f50d965eb2f6b5084d3f2
soapcandy <octi97@naver.com>
2023-07-13 12:33:25 +0900

    Update ReadComponent.js

--- src/components/products/ReadComponent.js
+++ src/components/products/ReadComponent.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 import { getProduct } from "../../api/productAPI";
 
 const initState = {
-    pno:0,
-    pname:'',
-    pdesc:'',
-    price:0,
-    images:[]
+    pno: 0,
+    pname: '',
+    pdesc: '',
+    price: 0,
+    images: []
 }
 
-const ReadComponent = ({pno}) => {
+const ReadComponent = ({ pno }) => {
 
     const [product, setProduct] = useState(initState)
 
@@ -20,28 +20,30 @@ const ReadComponent = ({pno}) => {
         })
     }, [pno])
 
-    return ( 
+    return (
         <div>
-            <div></div>
             <div className="m-2 p-2 border-2">
-                {product.pname}
-            </div>
-            <div className="m-2 p-2 border-2">
-                {product.pdesc}
-            </div>
-            <div className="m-2 p-2 border-2">
-                {product.price}
-            </div>
-            <div className="m-2 p-2 border-2">
-                <ul className="list-none">
-                    {product.images.map((fname,idx) => 
-                    <li key={idx}>
-                        <img src={`http://localhost/${fname}`} alt="빵통조림"></img>
-                    </li>)}
-                </ul>
+                <h1>Input</h1>
+                <div className="m-2 p-2 border-2">
+                    {product.pname}
+                </div>
+                <div className="m-2 p-2 border-2">
+                    {product.pdesc}
+                </div>
+                <div className="m-2 p-2 border-2">
+                    {product.price}
+                </div>
+                <div className="m-2 p-2 border-2">
+                    <ul className="list-none">
+                        {product.images.map((fname, idx) =>
+                            <li key={idx}>
+                                <img src={`http://localhost/${fname}`} alt="빵통조림"></img>
+                            </li>)}
+                    </ul>
+                </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default ReadComponent;
