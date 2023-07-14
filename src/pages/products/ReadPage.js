e6f7a87df5dbcdaba934db777d99df2bbc787fef
soapcandy <octi97@naver.com>
2023-07-14 15:29:28 +0900

    비 로그인 시 수정 페이지 접근 차단

--- src/pages/products/ReadPage.js
+++ src/pages/products/ReadPage.js
@@ -1,12 +1,17 @@
 import { useParams } from "react-router-dom"
 import useQueryObj from "../../hooks/useQueryObj"
 import ReadComponent from "../../components/products/ReadComponet"
+import useCustomLogin from "../../hooks/useCustomLogin"
 
 
 const ReadPage = () => {
 
     const {queryObj, setSearch,moveRead, moveList, moveModify} = useQueryObj()
     const {pno} = useParams()
+
+    useCustomLogin(() => {
+        alert("로그인 좀 부탁해요")
+    })
   
     console.log(pno)
     console.log(queryObj)
