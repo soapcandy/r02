fa68c64ae0273ace3376c601ade9bdd612fdf183
soapcandy <octi97@naver.com>
2023-07-18 12:31:04 +0900

    kakao 정보가져오기

--- src/components/member/KakaoLoginComponent.js
+++ src/components/member/KakaoLoginComponent.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 
 const REST_KEY='afda205eafa16e4bcbad4ef1fdca99aa'
-const REDIRECT_URI='http://localhost:8080/api/member/kakao'
+const REDIRECT_URI='http://localhost:3000/member/kakao'
 const kakaoURL=`https://kauth.kakao.com/oauth/authorize?client_id=${REST_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
 const KakaoLoginComponent = () => {
--- src/pages/member/KakaoRedirectPage.js
+++ src/pages/member/KakaoRedirectPage.js
@@ -1,6 +1,7 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
-import { getAccessToken, getUserEmail } from "../../api/kakaoAPI";
+// import { getAccessToken, getUserEmail } from "../../api/kakaoAPI";
 
 
 const KakaoRedirectPage = () => {
@@ -11,12 +12,14 @@ const KakaoRedirectPage = () => {
 
     useEffect(() => {
 
-        getAccessToken(authCode).then(accessToken => {
-            console.log(accessToken)
-            getUserEmail(accessToken).then(email => {
-                console.log("Email" + email)
-            })
-        }) 
+        axios.get(`http://localhost:8080/api/member/kakao?code=${authCode}`)
+
+        // getAccessToken(authCode).then(accessToken => {
+        //     console.log(accessToken)
+        //     getUserEmail(accessToken).then(email => {
+        //         console.log("Email" + email)
+        //     })
+        // }) 
     }, [authCode])
 
     return ( 
